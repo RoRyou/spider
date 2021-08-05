e1e548e44e5d5d4d21c9a1419255aa8219cb124e
RoRyou <luliang_penn@live.com>
2021-08-05 14:48:04 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,3 +1,4 @@
+import re
 import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -5,20 +6,22 @@ import random
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import re
 import mysql.connector
 import psycopg2
 import pandas as pd
-URL = "https://www.douyu.com/110/"
+import datetime
 
-
-
+URL = "https://www.douyu.com/22222"
+NUMMIN = 10
 pg_host = "192.168.2.154"
 pg_port = "5432"
 pg_user = "cy_t15_read"
 pg_password = "e7xwzt3CxJoxYNZ8"
 pg_database = "db_ana_t15"
 pg_table = "public.spider_dy"
+
+
+# REGULARTYPE = 10  # 每0（一直执行，一天做一次结算），10，30，60 分钟执行一次
 
 
 def gpcon(host, port, user, password, database, sql):
@@ -33,8 +36,6 @@ def gpcon(host, port, user, password, database, sql):
 
     curs.close()
     gpconn.close()
-
-
 
 
 def getText():
@@ -57,7 +58,7 @@ def removeLabel(commentList):
 
 
 def scrollScreen():
-    for i in range(1, 5):
+    for i in range(1, random.randint(2, 4)):
         browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
         time.sleep(random.randint(1, 3))
 
@@ -66,57 +67,102 @@ def getData():
     comments = browser.find_elements(By.CLASS_NAME, 'Barrage-notice--normalBarrage')
     return comments
 
+
 def genValues(texts):
     values = []
     for text in texts:
         nameword = text.split('：')
         name = nameword[0]
         word = nameword[1]
-        values.append(name)
-        values.append(word)
-    values = str(values).replace('[','(').replace(']',')')
+        washtime = str(datetime.datetime.now())
+        values.append([name, word, washtime, URL])
+
+    values = str(values).replace('[', '(').replace(']', ')')
+    values = values[1:-1]
     return values
+
+
+def regularTime():
+    # NUMMIN,Type = regularType(REGULARTYPE)
+    if (datetime.datetime.now().minute % 10 == 00) & (datetime.datetime.now().second == 00):
+        return True
+    else:
+        return False
+
+
+# # 设定时间进行定时
+# #返回3个值，
+# def regularType(REGULARTYPE):
+#     if REGULARTYPE == 0:
+#         NUMMIN = 24 * 60
+#         Type= 0
+#         return NUMMIN,Type
+#     elif REGULARTYPE == 10:
+#         NUMMIN = 10
+#         Type = 10
+#         return NUMMIN,Type
+#     elif REGULARTYPE == 30:
+#         NUMMIN = 30
+#         Type = 30
+#         return NUMMIN, Type
+#     elif REGULARTYPE == 60:
+#         NUMMIN = 60
+#         Type = 60
+#         return NUMMIN, Type
+
+def checkEndTime():
+    enddate = datetime.datetime.strptime(strEnd, '%Y-%m-%d')
+    if enddate > datetime.datetime.now():
+        return True
+    else:
+
+        return False
+
 
 if __name__ == '__main__':
-    browser = webdriver.Firefox()
-    browser.get(URL)
 
-    time.sleep(random.randint(5, 10))
+    strEnd = input("请输入截止时间(格式为Y-M-D):")
 
-    scrollScreen()
-    commentsList_ = getText()
-    browser.close()
-
-    #ETL
-    data = removeLabel(commentsList_)
-
-    values = genValues(data)
-
-    sql = f"insert into spider_dy(username,comment)values {values}"
-    print(sql)
-    print(data)
-    print(len(data))
-
-#     #sql
-#     gpcon(pg_host, pg_port, pg_user, pg_password, pg_database, pg_sql)
-# pg_sql = f"insert into spider_dy(username,comment)values {valuse}"
-# gpcon(pg_host, pg_port, pg_user, pg_password, pg_database, pg_sql)
+    if checkEndTime() is False:
+        print("截止时间小于当前时间，已中断")
 
 
-text = ['德拉诺什： 难道不是吗', '主播天问： 常乃超的原型确实是功德林的', '舌 七两牛肉二两面： 承认了', 'gutyyijp： 这个牙真实', '脑残喷子的野爹： 送走']
+    elif checkEndTime() is True:
+        print("程序开始执行")
+        while True:
 
+            if regularTime() is True:
 
-def genValues(texts):
-    values = []
-    for text in texts:
-        nameword = text.split('：')
-        name = nameword[0]
-        word = nameword[1]
-        values.append(name)
-        values.append(word)
-    values = str(values).replace('[','(').replace(']',')')
-    return values
+                print(datetime.datetime.now())
+                print('预计采集', NUMMIN, '分钟数据')
+                browser = webdriver.Firefox()
+                browser.get(URL)
 
+                scrollScreen()
 
-c = genValues(text)
-print(c)
+                time.sleep(NUMMIN * 60)
+
+                commentsList_ = getText()
+                browser.close()
+
+                data = removeLabel(commentsList_)
+                print('采集完毕')
+                print(len(data))
+
+                if len(data) == 0:
+                    print("无评论")
+
+                else:
+                    values = genValues(data)
+
+                    sql = f"insert into spider_dy(username,comment,washtime,url)values {values}"
+                    print(sql)
+
+                    # sql
+                    gpcon(pg_host, pg_port, pg_user, pg_password, pg_database, sql)
+
+                print(datetime.datetime.now())
+
+            elif checkEndTime() is False:
+                print("截止时间小于当前时间，已中断")
+                break
