df69fa135bddae44af7dd1e34a975aef8ab7e1f2
RoRyou <luliang_penn@live.com>
2021-08-11 15:01:18 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,3 +1,5 @@
+# coding=gbk
+
 import re
 import time
 from selenium import webdriver
@@ -10,18 +12,49 @@ import mysql.connector
 import psycopg2
 import pandas as pd
 import datetime
+import _thread
+import threading
 
-URL = "https://www.douyu.com/22222"
-NUMMIN = 10
-pg_host = "192.168.2.154"
-pg_port = "5432"
-pg_user = "cy_t15_read"
-pg_password = "e7xwzt3CxJoxYNZ8"
-pg_database = "db_ana_t15"
-pg_table = "public.spider_dy"
+from work.project.spider import spiderConfig
+
+# PARAMETER
+URL = spiderConfig.retURL()
+NUMMIN = spiderConfig.retIntTime()
+DURATION = spiderConfig.retDuration()
+
+pg_host, pg_port, pg_user, pg_password, pg_database, pg_table = spiderConfig.dbconfig()
+TIMESPLIT = spiderConfig.retTimeSplit()
 
 
-# REGULARTYPE = 10  # 每0（一直执行，一天做一次结算），10，30，60 分钟执行一次
+def inputStartEndTime():
+    try:
+        strStart = input("请输入开始日期(格式为Y-M-D)：")
+        strEnd = input("请输入截止日期(格式为Y-M-D)：")
+        startDate = datetime.datetime.strptime(strStart, '%Y-%m-%d')
+        endDate = datetime.datetime.strptime(strEnd, '%Y-%m-%d')
+
+        # if startDate == localTime().date():
+        #     startDate = localTime()
+
+        lTime = localTime()
+
+        if startDate < lTime:
+            print("Error:开始日期小于当前时间")
+        elif endDate < lTime:
+            print("Error:截止日期小于当前时间")
+        elif endDate < startDate:
+            print("Error:截止日期小于开始日期")
+        else:
+            return startDate, endDate
+    except IOError:
+        print("Error: 开始时间输入有误")
+
+
+def localTime():
+    lTime = datetime.datetime.now()
+    lTime = lTime.strftime('%Y-%m-%d %H:%M:%S')
+    lTime = datetime.datetime.strptime(lTime, '%Y-%m-%d %H:%M:%S')
+    return lTime
 
 
 def gpcon(host, port, user, password, database, sql):
@@ -38,14 +71,6 @@ def gpcon(host, port, user, password, database, sql):
     gpconn.close()
 
 
-def getText():
-    comments = browser.find_elements(By.CLASS_NAME, 'Barrage-notice--normalBarrage')
-    commentsList_ = []
-    for comment in comments:
-        commentsList_.append(comment.text)
-    return commentsList_
-
-
 def removeLabel(commentList):
     comtext = []
     for line in commentList:
@@ -55,17 +80,6 @@ def removeLabel(commentList):
         else:
             comtext.append(line)
     return comtext
-
-
-def scrollScreen():
-    for i in range(1, random.randint(2, 4)):
-        browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-        time.sleep(random.randint(1, 3))
-
-
-def getData():
-    comments = browser.find_elements(By.CLASS_NAME, 'Barrage-notice--normalBarrage')
-    return comments
 
 
 def genValues(texts):
@@ -82,87 +96,153 @@ def genValues(texts):
     return values
 
 
+# 定时
 def regularTime():
     # NUMMIN,Type = regularType(REGULARTYPE)
-    if (datetime.datetime.now().minute % 10 == 00) & (datetime.datetime.now().second == 00):
+    if datetime.datetime.now().minute % NUMMIN == 0:  # NUMMIN
         return True
     else:
         return False
 
 
-# # 设定时间进行定时
-# #返回3个值，
-# def regularType(REGULARTYPE):
-#     if REGULARTYPE == 0:
-#         NUMMIN = 24 * 60
-#         Type= 0
-#         return NUMMIN,Type
-#     elif REGULARTYPE == 10:
-#         NUMMIN = 10
-#         Type = 10
-#         return NUMMIN,Type
-#     elif REGULARTYPE == 30:
-#         NUMMIN = 30
-#         Type = 30
-#         return NUMMIN, Type
-#     elif REGULARTYPE == 60:
-#         NUMMIN = 60
-#         Type = 60
-#         return NUMMIN, Type
+def getData():
+    try:
+        browser = webdriver.Firefox()
+        browser.get(URL)
 
-def checkEndTime():
-    enddate = datetime.datetime.strptime(strEnd, '%Y-%m-%d')
-    if enddate > datetime.datetime.now():
-        return True
+        for i in range(1, random.randint(2, 4)):
+            time.sleep(random.randint(1, 3))
+            browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+
+        time.sleep(DURATION)  # 60
+
+        comments = browser.find_elements(By.CLASS_NAME, 'Barrage-notice--normalBarrage')
+        commentsList_ = []
+        for comment in comments:
+            commentsList_.append(comment.text)
+
+        browser.close()
+
+        data = removeLabel(commentsList_)
+        data = list(set(data))
+        print('采集完毕')
+        print(len(data))
+
+
+    except:
+        print("ERROR:数据采集问题")
+
+    try:
+        if len(data) == 0:
+            print("无评论")
+
+        else:
+            values = genValues(data)
+
+            sql = f"insert into spider_dy(username,comment,washtime,url)values {values}"
+            # print(sql)
+
+            # sql
+            gpcon(pg_host, pg_port, pg_user, pg_password, pg_database, sql)
+    except:
+        print("ERROR：数据加载问题")
+
+
+def localcheck(startDate, endDate):
+    if localTime() <= startDate:
+        return 0  # 时间未开始
+    elif (localTime() >= startDate) & (localTime() <= endDate):
+        return 1  # 开始
     else:
+        return 2  # 结束
 
-        return False
 
+def mainf():
+    while True:
+        if localTime().second % TIMESPLIT == 0:
+            print(localTime())
+
+        time.sleep(1)
+
+        # 每10分钟开始执行
+        if regularTime() is True:
+            if localTime().second % TIMESPLIT == 0:
+                print(localTime())
+            print("程序开始执行")
+            print('预计采集', NUMMIN, '秒数据')
+            getData()
+
+
+exitflag = 0
+
+
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("开始线程：" + self.name + '\n')
+
+        threadPro(self.name, self.counter)
+        print("结束线程：" + self.name + '\n')
+        print(localTime())
+
+
+def threadPro(threadName, counter):
+    while counter:
+        if exitflag:
+            threadName.exit()
+        getData()
+        # print("%s:%s"%(threadName,localTime()))
+        counter -= 1
+
+#threadflag = 1
+def threadLine():
+    threadflag = 1
+    threadNum = 1
+    while threadflag:
+        if (localTime().minute % NUMMIN == 0)&(localTime().second == 0):
+            print(localTime())
+            time.sleep(1)
+            threadName = 'thread' + str(threadNum)
+            threadName = myThread(threadNum, threadName, 1)
+            # if regularTime():
+            threadName.start()
+            threadNum += 1
+            threadflag = 0
+
+        else:
+            break
 
 if __name__ == '__main__':
+    # 时间输入检测
+    try:
+        startDate, endDate = inputStartEndTime()
 
-    strEnd = input("请输入截止时间(格式为Y-M-D):")
+    except:
+        print("ERROR:时间有误")
+    else:
+        print("时间输入无误，开始执行程序")
 
-    if checkEndTime() is False:
-        print("截止时间小于当前时间，已中断")
+    startDate = localTime() + datetime.timedelta(seconds=10)
+    print(startDate, endDate)
 
+    # if startDate == localTime().date():
+    #     startDate = localTime() + datetime.timedelta(seconds=10)
 
-    elif checkEndTime() is True:
-        print("程序开始执行")
-        while True:
+    # 时间范围检测
 
-            if regularTime() is True:
+    while 1:
+        if localTime().second % TIMESPLIT == 0:
+            print(localTime())
 
-                print(datetime.datetime.now())
-                print('预计采集', NUMMIN, '分钟数据')
-                browser = webdriver.Firefox()
-                browser.get(URL)
-
-                scrollScreen()
-
-                time.sleep(NUMMIN * 60)
-
-                commentsList_ = getText()
-                browser.close()
-
-                data = removeLabel(commentsList_)
-                print('采集完毕')
-                print(len(data))
-
-                if len(data) == 0:
-                    print("无评论")
-
-                else:
-                    values = genValues(data)
-
-                    sql = f"insert into spider_dy(username,comment,washtime,url)values {values}"
-                    print(sql)
-
-                    # sql
-                    gpcon(pg_host, pg_port, pg_user, pg_password, pg_database, sql)
-
-                print(datetime.datetime.now())
-
-            elif checkEndTime() is False:
-                print("截止时间小于当前时间，已中断")
-                break
+        time.sleep(1)
+        if localcheck(startDate, endDate) == 0:
+            pass
+        elif localcheck(startDate, endDate) == 1:
+            threadLine()
+        elif localcheck(startDate, endDate) == 2:
+            break
